810776110bd6e977d6d707884d5d4a66cb72e0dd
hazarath23 <125800907+hazarath23@users.noreply.github.com>
2025-01-13 13:03:15 +0530

    Add files via upload

---
+++ pi_3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<time.h>
+#include<omp.h>
+static long num_steps=100000;
+double step;
+#define NUM_THREADS 2
+int main()
+{
+  int i;double pi,sum=0.0;
+  step=1.0/(double)num_steps;
+  clock_t start_time = clock();
+  double x;
+  #pragma omp parallel for reduction(+:sum)
+  for(i=0;i<num_steps;i++)
+  {
+    x=(i+0.5)*step;
+    sum+=(4.0/(1.0+x*x));
+  }
+  pi=sum*step;
+  printf("%f\n",pi);
+  clock_t end_time = clock();
+  double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC; 
+  printf("Execution time: %f seconds\n", time_taken);
+  return 0;
+}
